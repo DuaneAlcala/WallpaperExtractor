efe8f38816bb3f210e4266bfdb231b2c659c82e4
Duane Alcala <duane.y.alcala@gmail.com>
2019-06-28 09:08:35 +1200

    Can establish a connection to website and farm posts

--- scraper.py
+++ scraper.py
@@ -2,3 +2,14 @@ import secrets
 
 import praw
 
+reddit_connection = praw.Reddit(client_id=secrets.client_id,
+                                client_secret=secrets.client_secret,
+                                user_agent=secrets.user_agent,
+                                username=secrets.username,
+                                password=secrets.password)
+
+subreddit = reddit_connection.subreddit('MostBeautiful')
+top = subreddit.top(limit=10)
+
+for post in subreddit.top(limit=10):
+    print(post.title, post.id)
